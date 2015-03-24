51b2521a581511fa3d99df2a7b667961d5b1f26f
Joshua A. Campbell <runguy@ymail.com>
2015-03-24 16:41:24 -0400

    reverted url script

--- raw urls/get_repos.py
+++ raw urls/get_repos.py
@@ -6,10 +6,10 @@ SLEEP_TIME_SECS = 1
 QUIT_COUNTER = 50000
 
 # Since
-since = 2533539
+since = 2649666
 
 # Counters
-counter = 1015014
+counter = 1065064
 new_counter = 0
 
 # A CSV file with username,password for your Github
@@ -17,23 +17,18 @@ credentials = ''.join(open('credentials.csv').readlines()).split(',')
 
 g = Github(credentials[0].strip(), credentials[1].strip())
 
-
 url_buffer = []
 last_id = None
 
-while True:
-    url_file = open('urls_' + str(counter+1) + '.txt', 'w+')
-    for repo in g.get_repos(since = since):
-        url_buffer.append( repo.html_url + '\n' )
+url_file = open('urls_' + str(counter+1) + '.txt', 'w+')
+for repo in g.get_repos(since = since):
+    url_buffer.append( repo.html_url + '\n' )
 
-        if len(url_buffer) > 1000:
-            url_file.writelines(url_buffer)
-            counter += len(url_buffer)
-            new_counter += len(url_buffer)
-            url_buffer = []
-            last_id = repo.id
-            print 'URLs written:', counter, 'Last id:', last_id
-            time.sleep(SLEEP_TIME_SECS)
-        if new_counter >= QUIT_COUNTER:
-            since = last_id
-            break
+    if len(url_buffer) > 1000:
+        url_file.writelines(url_buffer)
+        counter += len(url_buffer)
+        new_counter += len(url_buffer)
+        url_buffer = []
+        last_id = repo.id
+        print 'URLs written:', counter, 'Last id:', last_id
+        time.sleep(SLEEP_TIME_SECS)
