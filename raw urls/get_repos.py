84b67c48d585a64306f9ea0a56b53038cdbabc06
Joshua A. Campbell <runguy@ymail.com>
2015-03-30 00:31:47 -0400

    Updating w 6m urls and fixed auto repo crawl script

--- raw urls/get_repos.py
+++ raw urls/get_repos.py
@@ -2,14 +2,16 @@ import time
 
 from github import Github
 
-#SLEEP_TIME_SECS = 1
+STATUS_FILE = 'checkpoint.txt'
+EXCEPT_TIME_SECS = 5 * 60
+SLEEP_TIME_SECS = 1
 QUIT_COUNTER = 50000
 
 # Since
-since = 3280930
+since = 12255409
 
 # Counter
-counter = 1349348
+counter = 6059053
 
 # A CSV file with username,password for your Github
 credentials = ''.join(open('credentials.csv').readlines()).split(',')
@@ -17,23 +19,37 @@ credentials = ''.join(open('credentials.csv').readlines()).split(',')
 g = Github(credentials[0].strip(), credentials[1].strip())
 
 
-url_buffer = []
-last_id = None
-
 while True:
-    new_counter = 0
-    url_file = open('urls_' + str(counter+1) + '.txt', 'w+')
-    for repo in g.get_repos(since = since):
-        url_buffer.append( repo.html_url + '\n' )
+    try:
+        url_buffer = []
+        last_id = None
 
-        if len(url_buffer) > 1000:
-            url_file.writelines(url_buffer)
-            counter += len(url_buffer)
-            new_counter += len(url_buffer)
-            url_buffer = []
-            last_id = repo.id
-            print 'URLs written:', counter, 'Last id:', last_id
-            if new_counter >= QUIT_COUNTER:
+        new_counter = 0
+        url_file = open('urls_' + str(counter+1) + '.txt', 'w+')
+        
+        for repo in g.get_repos(since = since):
+            url_buffer.append( repo.html_url + '\n' )
+
+            if len(url_buffer) > 1000:
+                url_file.writelines(url_buffer)
+                counter += len(url_buffer)
+                new_counter += len(url_buffer)
+
+                url_buffer = []
+                last_id = repo.id
+
+                status = 'URLs written: ' + str(counter) + ' Last id: ' + str(last_id)
+                print status
+                
+                f = open(STATUS_FILE, 'w+')
+                f.write(status)
+                f.close()
+                
                 since = last_id
-                break
-            #time.sleep(SLEEP_TIME_SECS)
+
+                if new_counter >= QUIT_COUNTER:
+                    break
+                time.sleep(SLEEP_TIME_SECS)
+    except:
+        print 'Exception caught. Now sleeping'
+        time.sleep(EXCEPT_TIME_SECS)
