cca2f7f4bcea584fc9ceb3c33fbdd61cef116916
Joshua A. Campbell <runguy@ymail.com>
2015-04-12 00:51:14 -0400

    Created readme index for clean readmes

--- data/build_readme_index_list.py
+++ data/build_readme_index_list.py
@@ -5,8 +5,8 @@ import os
 Builds a JSON array containing the indices that have been samples.
 """
 if __name__ == '__main__':
-    RAW_DIR = 'raw'
-    OUTPUT_FILE = 'readme_index_list.json'
+    README_DIR = 'clean'
+    OUTPUT_FILE = 'clean_readme_index_list.json'
     
 
     all_readmes = []
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     # Looping manually is faster because we do not need to do lots of isFile()
     # checks.
     print 'Finding READMEs.'
-    for d in os.listdir(RAW_DIR):
-        for p in os.listdir(os.path.join(RAW_DIR, d)):
+    for d in os.listdir(README_DIR):
+        for p in os.listdir(os.path.join(README_DIR, d)):
             all_readmes.append(int(p.split(os.path.sep)[-1].split('.')[0]))
 
     print 'Sorting.'
